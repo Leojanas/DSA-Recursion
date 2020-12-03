deaab85510c2517f7b9101cea89cd6a97cd442a5
Leo Janas <leojanas7@gmail.com>
2020-12-03 11:57:18 -0700

    Implement powerCalculator and reverseString

--- assignment.js
+++ assignment.js
@@ -7,3 +7,30 @@ function countSheep(number){
     countSheep(number)
 }
 countSheep(4)
+
+function powerCalculator(base, exponent){
+    if(exponent < 0){
+        return console.log('Exponent should be >= 0.')
+    }
+    if(exponent === 0){
+        return console.log(1)
+    }
+    if(exponent === 1){
+        return base
+    }else{
+        exponent = exponent-1;
+        return base*powerCalculator(base, exponent)
+    }
+}
+console.log(powerCalculator(10,1))
+
+function reverseString(string){
+    if(string.length === 1){
+        return string
+    }
+    let newString = string.substring(0, string.length - 1)
+    let char = string.charAt(string.length -1)
+    return char.concat(reverseString(newString))
+}
+console.log(reverseString('Hello'))
+
